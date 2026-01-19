4ed6ae90447d6d5393f3cffce3059db24a82887d
AruukeNurpaiaskyzy <aruukelisa@gmail.com>
2026-01-20 01:51:38 +0600

    indexes

--- main.py
+++ main.py
@@ -1,20 +1,28 @@
 from fastapi import FastAPI, HTTPException
 from project.models import Movie
+from typing import List
+from pydantic import BaseModel
 # from pydantic import BaseModel
 # from typing import Optional, Annotated
 # from fastapi import Depends, HTTPException
 
-app = FastAPI(title = 'Movies API')
+app = FastAPI()
+
+class Movie(BaseModel):
+    id: int
+    title: str
+    description: str
+
+# Пример базы данных
 movies_db = [
-    Movie(title='Movie 1', year=2021, description='Movie 1' ),
-    Movie(title='Movie 2', year=2022, description='Movie 2' ),
-    Movie(title='Movie 3', year=2023, description='Movie 3' ),
-    Movie(title='Movie 4', year=2024, description='Movie 4' ),
-    Movie(title='Movie 5', year=2025, description='Movie 5' ),
+    Movie(id=1, title="Фильм 1", description="Описание 1"),
+    Movie(id=2, title="Фильм 2", description="Описание 2"),
+    Movie(id=3, title="Фильм 3", description="Описание 3"),
 ]
-@app.get('/')
-def read_root():
-    return{'Hello': 'Aruuke'}
+
+# @app.get('/')
+# def read_root():
+#     return{'Hello': 'Aruuke'}
 # class Task(BaseModel):
 #     name: str
 #     description: Optional [str] = None
@@ -40,16 +48,30 @@ def read_root():
 #     task = Task(name = "давай создадим проект")
 #     return {"data": task}
 
-@app.get('/movies/', response_model=list[Movie])
-def get_movie():
+# @app.get('/movies/', response_model=list[Movie])
+# def get_movie():
+#     return movies_db
+# # class Task(BaseModel):
+
+# @app.get('/movies{movie_id}/', response_model=list[Movie])
+# def get_movie_by_id(movie_id:int):
+#     if movie_id < 0 or movie_id > len(movies_db):
+#         raise HTTPException(status_code=404, detail='Movie not found')
+#     return movies_db[movie_id - 1]
+# # class Task(BaseModel):
+
+
+@app.get('/movies/', response_model=List[Movie])
+def get_movies():
     return movies_db
-# class Task(BaseModel):
 
-@app.get('/movies{movie_id}/', response_model=list[Movie])
-def get_movie_by_id(movie_id:int):
-    if movie_id < 0 or movie_id > len(movies_db):
-        raise HTTPException(status_code=404, detail='Movie not found')
-    return movies_db[movie_id]
-# class Task(BaseModel):
-
+@app.get('/movies/{movie_id}', response_model=Movie)
+def get_movie_by_id(movie_id: int):
+    # Ищем фильм по ID (а не по индексу массива)
+    for movie in movies_db:
+        if movie.id == movie_id:
+            return movie
+    
+    # Если не нашли, выбрасываем исключение
+    raise HTTPException(status_code=404, detail='Movie not found')
 
