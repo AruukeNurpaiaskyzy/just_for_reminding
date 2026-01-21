b9ffa8b2a27c8061cd312ccc24973fb95f5eaad8
AruukeNurpaiaskyzy <aruukelisa@gmail.com>
2026-01-21 20:24:47 +0600

    example

--- project/models.py
+++ project/models.py
@@ -1,8 +1,10 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
+from typing import Optional
 
 
 
-class Movie (BaseModel):
-    title: str
-    year: int
-    description: str
+class Movie(BaseModel):
+    title: str = Field(..., example='Смурфики', description='Movie title')
+    year: int = Field(..., example=2011, description='Release year')
+    description: str = Field(..., example='Приключения смурфиков в лесу', 
+                           description='Movie description')
